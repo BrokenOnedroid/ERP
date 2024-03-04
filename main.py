e632095bc4d7e22bd0218ed866230656a7730bea
Vincent Kautz <vincent.kautz@ccontor.de>
2024-03-04 12:10:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+# Standard library imports
+import sys
+
+# GUI Imports
+from tkinter import *
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue") 
+
+# Main Window
+class App(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.header_font = customtkinter.CTkFont(family="Helvetica", size=44, weight="bold", slant="roman", underline=False, overstrike=False)
+        self.geometry("1200x800")
+        self.minsize(400, 400)
+        self.title("ERP")
+        # self.set_appearance_mode("dark")
+        # self.set_default_color_theme("dark-green")
+        
+        self.title_label = customtkinter.CTkLabel(self, text='Hauptmenü', fg_color='transparent', font=self.header_font)
+        self.title_label.pack(padx=20, pady=20, anchor=customtkinter.CENTER)
+        
+        self.article_menue_button = customtkinter.CTkButton(self, text="Artikelübersicht", command=self.open_article_window)
+        self.article_menue_button.pack(padx=20, pady=20)
+        
+        self.inventory_button = customtkinter.CTkButton(self, text="Inventar", command=self.open_inventar_window)
+        self.inventory_button.pack(padx=20, pady=20)
+
+        self.inventory_button = customtkinter.CTkButton(self, text="Beenden", command=self.close_window)
+        self.inventory_button.pack(padx=20, pady=20)
+
+        self.toplevel_window = None
+
+    def open_inventar_window(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window  = Inventory(self)  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus_set()  # if window exists focus it
+    
+    def open_article_window(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window  = Article()  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus_set()  # if window exists focus it
+    
+    def close_window(self):
+        self.destroy()
+
+# inventory Windows
+class Inventory(customtkinter.CTkToplevel):
+	
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.header_font = customtkinter.CTkFont(family="Helvetica", size=18, weight="bold", slant="roman", underline=False, overstrike=False)
+        self.title("Inventar")
+        self.geometry("1000x500")
+#        self.inventory_window.resizable(True, True) # Width, Height
+        
+        self.title_label = customtkinter.CTkLabel(self, text='Inventar', fg_color='transparent', font=self.header_font)
+        self.title_label.pack(padx=20, pady=20, anchor=customtkinter.CENTER)
+
+        # Close the window
+        self.new_button = customtkinter.CTkButton(self, text="Close Window", command=self.close)
+        self.new_button.pack(pady=40)
+    
+    def close(self):
+        self.destroy()
+
+class Article(customtkinter.CTkToplevel):
+    pass
+
+# Main Function
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
